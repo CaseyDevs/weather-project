65ef44d0da68fe6e500d46e00bbb978355284baa
Casey <69276912+CaseyDevs@users.noreply.github.com>
2023-01-29 00:55:38 +0000

    JS Code Cleanup, Heroku compatibility

--- server.js
+++ server.js
@@ -26,9 +26,6 @@ app.use(limiter)
 // Set the view engine to EJS
 app.set("view engine", "ejs")
 
-// Gather apiKey from the enviroment file
-const apiKey = process.env.API_KEY
-
 // *** Displays ***
 
 // Index page
@@ -44,78 +41,76 @@ app.get("/about", (req, res) => {
 // *** Post request display ***
 
 app.post("/", (req, res) => {  
-    let city = req.body.city // Get city name passed from form input
-    let url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey
+    const city = req.body.city // Get city name passed from form input
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}`
 
     // *** Request data from the API ***
 
     request(url, (err, response, body) => {
 
-    // Check if JSON data is fetched successfully on return
-    if (err) {
-        res.render("pages/index", {weather: null, error: 'Error fetching the correct data, try again...'})
-    }
-    else {
-        let weather = JSON.parse(body)
-        // *** Check if any API data is returned undefined ***
-
-        console.log(weather); 
-
-        if (weather.main == undefined) {
-            res.render("pages/index", {weather: null, error: "Could not find data for this location, please try again..."})
-        }
-        else {
-            // Use recieved data for output
-            let place = weather.name
-            let weatherTemp = roundTemp(weather.main.temp) + "°"
-            let weatherPressure = weather.main.pressure
-            let iconTag = weather.weather[0].icon
-            let weatherIcon = "http://openweathermap.org/img/wn/" + iconTag + "@2x.png"
-            let weatherDesctiption = weather.weather[0].description
-            let splitWeatherDescription
-
-            // Capitalise each word in weather description if description contains a space
-            if (weatherDesctiption.includes(" ")) {
-                splitWeatherDescription = weatherDesctiption.split(" ")
-                for(let i = 0; i < splitWeatherDescription.length; i++) {
-                    splitWeatherDescription[i] = splitWeatherDescription[i][0].toUpperCase() + splitWeatherDescription[i].substr(1)
-                }
-                splitWeatherDescription = splitWeatherDescription.join(" ")
+        // Check if JSON data is fetched successfully on return
+        if (err) {
+            res.render("pages/index", {weather: null, error: 'Error fetching the correct data, try again...'})
+        } else {
+            const weather = JSON.parse(body)
+        
+            // *** Check if any API data is returned undefined ***
+            if (weather.main == undefined) {
+                res.render("pages/index", {weather: null, error: "Could not find data for this location, please try again..."})
             } else {
-                splitWeatherDescription = weatherDesctiption
+                // Use recieved data for output
+                const place = weather.name
+                const weatherTemp = roundTemp(weather.main.temp) + "°"
+                const weatherPressure = weather.main.pressure
+                const weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+                const weatherDesctiption = weather.weather[0].description
+                let splitWeatherDescription
+
+                // Capitalise each word in weather description if description contains a space
+                if (weatherDesctiption.includes(" ")) {
+                    splitWeatherDescription = weatherDesctiption.split(" ")
+
+                    for (let i = 0; i < splitWeatherDescription.length; i++) {
+                        splitWeatherDescription[i] = splitWeatherDescription[i][0].toUpperCase() + splitWeatherDescription[i].substr(1)
+                    }
+
+                    splitWeatherDescription = splitWeatherDescription.join(" ")
+
+                } else {
+                    splitWeatherDescription = weatherDesctiption
+                    splitWeatherDescription[0].toUpperCase()
+                }
+
+                const humidity = weather.main.humidity
+                const clouds = weather.clouds.all
+                const visibility = weather.visibility
+                const main = weather.weather[0].main
+
+                // Rounds temperature to 2 decimal places using exponents
+                function roundTemp (number) {
+                    return +(Math.round(number))
+                }
+
+                // *** Render values to the webpage, generating a static page which is displayed to the user
+
+                res.render("pages/index", {
+                    weather: weather,
+                    place: place,
+                    temp: weatherTemp,
+                    pressure: weatherPressure,
+                    icon: weatherIcon,
+                    description: splitWeatherDescription,
+                    humidity: humidity,
+                    clouds: clouds,
+                    visibility: visibility,
+                    main: main,
+                    error: null,
+                })
             }
-
-            let humidity = weather.main.humidity
-            let clouds = weather.clouds.all
-            let visibility = weather.visibility
-            let main = weather.weather[0].main
-            let weatherFahrenheit = (weatherTemp * 9) / 5 + 32 // Converts Celecius to Fahrenheit
-
-            // Rounds temoperature to 2 decimal places using exponents
-            function roundTemp (number) {
-                return +(Math.round(number)) // Rounds num to 2dp and "e-2" bring the number back to its original format with 2dp
-            }
-            weatherFahrenheit = roundTemp(weatherFahrenheit)
-
-            // *** Render values to the webpage, generating a static page which is displayed to the user
-            
-            res.render("pages/index", {
-                weather: weather,
-                place: place,
-                temp: weatherTemp,
-                pressure: weatherPressure,
-                icon: weatherIcon,
-                description: splitWeatherDescription,
-                humidity: humidity,
-                fahrenheit: weatherFahrenheit,
-                clouds: clouds,
-                visibility: visibility,
-                main: main,
-                error: null,
-            })}
-        }})
+        }
     })
-// *** Server port config ***
-app.listen(3000, () => {
-    console.log("Weather project listening on port 3000");
+    
 })
+
+// *** Port ***
+app.listen(process.env.PORT || 3000)
