a8f9d656ee2a05e96864b158427ab21154243185
Casey <69276912+CaseyDevs@users.noreply.github.com>
2023-01-29 02:24:34 +0000

    General Improvements

--- public/scripts/navigationLinks.js
+++ public/scripts/navigationLinks.js
@@ -1,7 +1,6 @@
 function setActiveNavigation () {
     let path = window.location.href
     let navLinks = document.querySelectorAll("li a")
-    console.log(navLinks);
      
     navLinks.forEach(navLink => {
         if (navLink.href === path) {
